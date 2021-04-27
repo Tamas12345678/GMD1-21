abbaba19bea1467e2b88977bdf5b5b20ba499ddc
Tamas12345678 <43003631+Tamas12345678@users.noreply.github.com>
2021-04-27 10:58:03 +0200

    First Commit
    -Main menu
    -lvl1 started

---
+++ TopDownShooter/Assets/Scripts/Lvl1/RoomTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] TopRooms;
+    public GameObject[] BottomRooms;
+    public GameObject[] RightRooms;
+    public GameObject[] leftRooms;
+    
+}
