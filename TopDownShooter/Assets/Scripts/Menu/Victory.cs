7b2aa425ed3ee8ebe11842aa5e2b5ea5952f7b59
Tamas12345678 <43003631+Tamas12345678@users.noreply.github.com>
2021-05-07 18:59:05 +0200

    Victory Scene
    Buttons added with responsive functions

--- TopDownShooter/Assets/Scripts/Menu/Victory.cs
+++ TopDownShooter/Assets/Scripts/Menu/Victory.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Victory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void TryAgain()
     {
-        
+        SceneManager.LoadScene(0);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void BackToMainMenu()
     {
-        
+        SceneManager.LoadScene(2);
     }
 }
